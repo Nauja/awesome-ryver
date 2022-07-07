74d17fb99713be21f2e548b54caaf2dbb0638c2a
Nauja <jeremymorosi@hotmail.fr>
2022-07-07 22:24:58 +0200

    images

--- awesome_ryver/__main__.py
+++
@@ -1,5 +0,0 @@
-from awesome_ryver import main
-    
-
-if __name__ == "__main__":
-    main()
--- test/test_main.py
+++
@@ -1,7 +0,0 @@
-from awesome_ryver import run
-import pytest
-
-
-def test_run():
-    with pytest.raises(Exception):
-        run(timeout=5)
